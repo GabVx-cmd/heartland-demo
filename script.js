bb528bbd05d8ec81c19220480fb6d0f1f635ce6c
GabVx-cmd <gabzdeleon1001@gmail.com>
2025-12-07 10:40:05 +0800

    Improve web page loading experience by adding preloader and fixing hover styles

---
+++ script.js
@@ -0,0 +1,14 @@
+var loader = document.getElementById("preloader");
+
+window.addEventListener("load", function(){
+    loader.style.display = "none";
+});
+
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
